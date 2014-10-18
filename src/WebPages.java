7c2668d7e7b583b0181057b8ff03d940690a1db1
Bobby Signor <signor.bobby@gmail.com>
2014-10-18 00:35:28 -0600

    Change termIndex to be a BST

--- src/WebPages.java
+++ src/WebPages.java
@@ -14,7 +14,7 @@ public class WebPages {
     /**
      * The sorted list of terms encountered across all documents.
      */
-    private ArrayList<Term> termIndex = new ArrayList<Term>();
+    private BST<Term> termIndex = new BST<Term>();
     
     /**
      * A counter used for grading purposes.
@@ -40,7 +40,7 @@ public class WebPages {
             while(scanFile.hasNext()) {                               //While there's still stuff left in the document...
                 String s = scanFile.next();
                 if (!s.equals(""))
-                    addWord(new Term(s, fileName));       //...take the word, make it into a term,
+                    this.termIndex.add(new Term(s, fileName));       //...take the word, make it into a term,
             }
             scanFile.close();
         } catch (FileNotFoundException e) {
@@ -55,33 +55,7 @@ public class WebPages {
      * @author Bobby Signor
      */
     private void addWord(Term newTerm) {
-        int start = 0, end = this.termIndex.size() - 1, middle = start + ((end - start) / 2);
-
-        //The binary insertion logic.
-        while (end >= start) {
-            if (this.termIndex.get(middle).compareTo(newTerm) < 0) {
-                //If we're not far enough into the alphabet, set the next zone to start at middle + 1.
-                start = middle + 1;
-            } else if (this.termIndex.get(middle).compareTo(newTerm) > 0) {
-                //If we're too far into the alphabet, set the next zone to end at middle - 1.
-                end = middle - 1;
-            } else {                                //If the word is already in the list...
-                this.termIndex.get(middle).incFrequency(newTerm.getDocsList().get(0).getDocName());
-                //...then we can leave. So leave.
-                return;
-                //What are you still doing here?
-                //No, seriously, you should leave.
-                //Word's already been dealt with. There's nothing left to do in this section.
-                //Wait, you're just reading straight through my code, aren't you?
-                //God, that really sucks for you, doesn't it?
-                //Well, go on. There's nothing important in these comments. You can ignore them.
-                //I'm serious. I'm just doing typing these to get some practice on a new keyboard.
-                //Okay, see, I wasn't kidding in the last line. I'm 100% just doing this to get some more practice.
-                //Alright, fine. I'm done. See? Next line isn't a comment. You're free. Congrats.
-            }
-            middle = start + ((end - start) / 2);
-        }
-        this.termIndex.add(middle, newTerm);
+        this.termIndex.add(newTerm);
     }
 
     /**
@@ -91,21 +65,7 @@ public class WebPages {
      * @author Bobby Signor
      */
     public Term findWord(String name) {
-        int start = 0, end = this.termIndex.size() - 1, middle = start + ((end - start) / 2);
-        Term newTerm = new Term(name);
-
-        while (end >= start) {
-            if (this.termIndex.get(middle).compareTo(newTerm) < 0) {
-                start = middle + 1;
-            } else if (this.termIndex.get(middle).compareTo(newTerm) > 0) {
-                end = middle - 1;
-            } else {
-                return this.termIndex.get(middle);
-            }
-            middle = start + ((end - start) / 2);
-        }
-
-        return null;
+        return this.termIndex.get(new Term(name), false);
     }
 
     /**
@@ -129,125 +89,13 @@ public class WebPages {
     }
 
     /**
-     * Prunes a given number of words.
-     * @param n The number of words to prune.
-     * @author Grant Stein
-     */
-    public void pruneStopWords(int n){                      //remove a certain number of determined stopwords from the arraylist
-        this.timesMerged = 0;
-        mergeSortByCount(0, termIndex.size() - 1);          //sort terms by total # of occurrences
-        System.out.println("Copies: " + this.timesMerged);
-        for(int i = 0; i < n; i++){                         //remove the number of stop words
-            termIndex.remove(termIndex.size() - 1);
-        }
-        this.timesMerged = 0;
-        mergeSortByName(0, termIndex.size() - 1);           //re-sort the terms alphabetically
-        System.out.println("Copies: " + this.timesMerged + "\n");
-    }
-
-    /**
-     * Performs a Merge Sort on termIndex by total frequency.
-     * @param first The first element in the section.
-     * @param last The last element in the section.
-     * @author Bobby Signor
-     */
-    private void mergeSortByCount(int first, int last) {
-        if (last - first > 0) {
-            int middle = first + ((last - first) / 2);
-            mergeSortByCount(first, middle);
-            mergeSortByCount(middle + 1, last);
-            mergeByCount(first, middle, middle + 1, last);
-        }
-    }
-
-    /**
-     * Merges 2 sections of the array back together in ascending order by total frequency.
-     * @param s1Front The front of the first section of the array.
-     * @param s1Back The back of the first section of the array.
-     * @param s2Front The front of the second section of the array.
-     * @param s2Back The back of the second section of the array.
-     * @author Bobby Signor
-     */
-    private void mergeByCount(int s1Front, int s1Back, int s2Front, int s2Back) {
-        int s1Current = s1Front, s2Current = s2Front;
-        ArrayList<Term> temp = new ArrayList<Term>(s2Back - s1Front);
-
-        while (s1Current <= s1Back && s2Current <= s2Back) {
-            if (this.termIndex.get(s1Current).getTotalFrequency() <= this.termIndex.get(s2Current).getTotalFrequency()) {
-                this.timesMerged++;
-                temp.add(this.termIndex.get(s1Current));
-                s1Current++;
-            } else {
-                temp.add(this.termIndex.get(s2Current));
-                s2Current++;
-            }
-        }
-
-        while (s1Current <= s1Back)
-            temp.add(this.termIndex.get(s1Current++));
-
-        while (s2Current <= s2Back)
-            temp.add(this.termIndex.get(s2Current++));
-
-        for (int i = 0; i < temp.size(); i++)
-            this.termIndex.set(i + s1Front, temp.get(i));
-    }
-
-    /**
-     * Performs a Merge Sort on termIndex in alphabetical order.
-     * @param first The first element in the section.
-     * @param last The last element in the section.
-     * @author Bobby Signor
-     */
-    private void mergeSortByName(int first, int last) {
-        if (last - first > 0) {
-            mergeSortByName(first, first + ((last - first) / 2));
-            mergeSortByName((first + ((last - first) / 2)) + 1, last);
-            mergeByName(first, first + ((last - first) / 2), (first + ((last - first) / 2)) + 1, last);
-        }
-    }
-
-    /**
-     * Merges 2 sections of the array back together in ascending alphabetical order.
-     * @param s1Front The front of the first section of the array.
-     * @param s1Back The back of the first section of the array.
-     * @param s2Front The front of the second section of the array.
-     * @param s2Back The back of the second section of the array.
-     * @author Bobby Signor
-     */
-    private void mergeByName(int s1Front, int s1Back, int s2Front, int s2Back) {
-                int s1Current = s1Front, s2Current = s2Front;
-        ArrayList<Term> temp = new ArrayList<Term>(s2Back - s1Front);
-
-        while (s1Current <= s1Back && s2Current <= s2Back) {
-            if (this.termIndex.get(s1Current).compareTo(this.termIndex.get(s2Current)) <= 0) {
-                this.timesMerged++;
-                temp.add(this.termIndex.get(s1Current));
-                s1Current++;
-            } else {
-                temp.add(this.termIndex.get(s2Current));
-                s2Current++;
-            }
-        }
-
-        while (s1Current <= s1Back)
-            temp.add(this.termIndex.get(s1Current++));
-
-        while (s2Current <= s2Back)
-            temp.add(this.termIndex.get(s2Current++));
-
-        for (int i = 0; i < temp.size(); i++)
-            this.termIndex.set(i + s1Front, temp.get(i));
-    }
-
-    /**
      * Prints the Terms in termIndex in PA2's format
      * @author Grant Stein
      */
     public void printTerms(){
         System.out.println("WORDS");
-        for (int i = 0; i < termIndex.size(); i++) {
-            System.out.println(termIndex.get(i));
+        for (Term t : this.termIndex) {
+            System.out.println(t);
         }
         System.out.println();
     }
@@ -333,10 +181,6 @@ public class WebPages {
 
         public int size() {
             return this.size;
-        }
-
-        public boolean isEmpty() {
-            return this.size == 0;
         }
 
         /**
